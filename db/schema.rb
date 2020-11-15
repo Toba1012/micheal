2e45708760917075a9a32e53246d3d7781814c3a
Toba <marxdm02@gmail.com>
2020-11-15 14:03:28 +0000

    スタンプ機能

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_100144) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "profile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
